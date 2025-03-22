5094440a6c372d67a5fcb3ebab089684f05fdecd
LilToadFood <163685847+LilToadFood@users.noreply.github.com>
2025-03-22 15:28:03 -0400

    Add files via upload

--- grizzhacks3.py
+++ grizzhacks3.py
@@ -2,18 +2,30 @@ import pygame
 
 pygame.init()
 
+##Constants
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+LAKE_WIDTH = 400
+LAKE_HEIGHT = 300
+
+#our screen to display on
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-player = pygame.Rect((300,200,50,50))
+#variables such for our objects
+player = pygame.Rect((400,50,50,50))
+water = pygame.Rect((SCREEN_WIDTH/2-LAKE_WIDTH/2, SCREEN_HEIGHT/2-LAKE_HEIGHT/2, LAKE_WIDTH, LAKE_HEIGHT))
 
+#the loop the game runs in
 run = True
 while run:
 
     screen.fill("green")
 
+    #draws the lake and player on the screen
     pygame.draw.rect(screen, (255,0,0), player)
+    pygame.draw.rect(screen, (55,55,255), water)
+
+    #this is our key inputs
     key = pygame.key.get_pressed()
     if key[pygame.K_LEFT]:
         player.move_ip(-1,0)
@@ -24,9 +36,11 @@ while run:
     elif key[pygame.K_DOWN]:
         player.move_ip(0,1)
 
+    #allows you to close the aplication
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     pygame.display.update()
 
+#ends the game
 pygame.quit() 
